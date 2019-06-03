9fde1c612ba7c215507d2bfa1d029b855b7835e5
candra Kriswinarto <ckriswinarto@gmail.com>
2019-06-03 11:19:29 +0800

    Create google map API and the contact form and the footer section

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+// Google Map
+function initMap() {
+    // Map options
+    let options = {
+        zoom: 8,
+        center: {lat: 48.55, lng: -3.15}
+    }
+
+    // New map
+    let map = new google.maps.Map(document.querySelector('.map'), options);
+
+    // Add marker
+    let marker = new google.maps.Marker({
+        position: {lat: 48.55, lng: -3.15}
+    });
+}
